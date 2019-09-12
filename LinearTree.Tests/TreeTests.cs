296aa24871f5b5027b8eebb1010bbfa588a4831b
Бессонов Дмитрий <zawodskoj@outlook.com>
2019-09-12 19:43:09 +0300

    initial commit

---
+++ LinearTree.Tests/TreeTests.cs
@@ -0,0 +1,361 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Schema;
+using Xunit;
+
+namespace LinearTree.Tests
+{
+    public class TreeTests
+    {
+        private List<T> CreateAutoDispatchedList<T>(LinearTree<T> tree)
+        {
+            var list = new List<T>(tree);
+
+            tree.CollectionChanged += (_, change) =>
+            {
+                switch (change.Type)
+                {
+                    case CollectionChangeType.INSERT:
+                        list.InsertRange(change.DestinationIndex, tree.Skip(change.DestinationIndex).Take(change.Count));
+                        break;
+                    case CollectionChangeType.REMOVE:
+                        list.RemoveRange(change.SourceIndex, change.Count);
+                        break;
+                    case CollectionChangeType.MOVE:
+                        var src = change.SourceIndex;
+                        var dst = change.DestinationIndex;
+                        var n = change.Count;
+
+                        if (src == dst) break;
+                        if (src < dst)
+                        {
+                            for (int i = src + n - 1, j = dst + n - 1; i >= src; i--, j--)
+                            {
+                                var el = list[i];
+                                list.RemoveAt(i);
+                                list.Insert(j, el);
+                            }
+                        }
+                        else
+                        {
+                            for (int i = src, j = dst; i < src + n; i++, j++)
+                            {
+                                var el = list[i];
+                                list.RemoveAt(i);
+                                list.Insert(j, el);
+                            }
+                        }
+                        
+                        break;
+                    case CollectionChangeType.REPLACE:
+                        for (var i = change.SourceIndex; i < change.SourceIndex + change.Count; i++)
+                            list[i] = tree[i];
+                        break;
+                }
+            };
+
+            return list;
+        }
+
+        private LinearTree<int> GenerateTestTree()
+        {
+            var tree = new LinearTree<int>();
+            
+            // @formatter:off
+            var n00 = tree.InsertNode(00, 0);
+                var n01 = n00.InsertNode(01, 0);
+            var n02 = tree.InsertNode(02, 1);
+                var n03 = n02.InsertNode(03, 0);
+                    var n04 = n03.InsertNode(04, 0);
+                var n05 = n02.InsertNode(05, 1);
+                var n06 = n02.InsertNode(06, 2);
+            var n07 = tree.InsertNode(07, 2);
+            var n08 = tree.InsertNode(08, 3);
+                var n09 = n08.InsertNode(09, 0);
+                    var n10 = n09.InsertNode(10, 0);
+                        var n11 = n10.InsertNode(11, 0);
+            var n12 = tree.InsertNode(12, 4);
+                var n13 = n12.InsertNode(13, 0);
+            var n14 = tree.InsertNode(14, 5);
+                var n15 = n14.InsertNode(15, 0);
+                var n16 = n14.InsertNode(16, 1);
+            // @formatter:on
+
+            return tree;
+        }
+
+        [Fact]
+        public void TestTreeMatchesListRepresentation()
+        {
+            Assert.Equal(GenerateTestTree(), Enumerable.Range(0, 17));
+        }
+
+        [Fact]
+        public void AutoDispatchedInsertMatchesTree()
+        {
+            var testTree = GenerateTestTree();
+            var autoList = CreateAutoDispatchedList(testTree);
+            var manualList = Enumerable.Range(0, 17).ToList();
+
+            testTree.Children[4].InsertNode(-1, 1);
+            manualList.Insert(14, -1);
+
+            Assert.Equal(manualList.Count, testTree.Count);
+            Assert.Equal(manualList, testTree);
+            Assert.Equal(manualList, autoList);
+            
+            testTree.InsertNode(-2, 6);
+            manualList.Insert(18, -2);
+
+            Assert.Equal(manualList.Count, testTree.Count);
+            Assert.Equal(manualList, testTree);
+            Assert.Equal(manualList, autoList);
+        }
+
+        [Fact]
+        public void AutoDispatchedRemoveMatchesTree()
+        {
+            var testTree = GenerateTestTree();
+            var autoList = CreateAutoDispatchedList(testTree);
+            var manualList = Enumerable.Range(0, 17).ToList();
+
+            testTree.Children[4].RemoveNode(0);
+            manualList.RemoveAt(13);
+
+            Assert.Equal(manualList.Count, testTree.Count);
+            Assert.Equal(manualList, testTree);
+            Assert.Equal(manualList, autoList);
+            
+            testTree.RemoveNode(1);
+            manualList.RemoveRange(2, 5);
+
+            Assert.Equal(manualList.Count, testTree.Count);
+            Assert.Equal(manualList, testTree);
+            Assert.Equal(manualList, autoList);
+        }
+        
+        [Fact]
+        public void AutoDispatchedMoveMatchesTree()
+        {
+            var testTree = GenerateTestTree();
+            var autoList = CreateAutoDispatchedList(testTree);
+            var manualList = Enumerable.Range(0, 17).ToList();
+
+            testTree.MoveNode(5, 3);
+            manualList.Insert(8, 14);
+            manualList.Insert(9, 15);
+            manualList.Insert(10, 16);
+            manualList.RemoveRange(17, 3);
+
+            Assert.Equal(manualList.Count, testTree.Count);
+            Assert.Equal(manualList, testTree);
+            Assert.Equal(manualList, autoList);
+            
+            testTree.Children[1].MoveNode(0, 1);
+            manualList.RemoveRange(3, 2);
+            manualList.Insert(4, 3);
+            manualList.Insert(5, 4);
+
+            Assert.Equal(manualList.Count, testTree.Count);
+            Assert.Equal(manualList, testTree);
+            Assert.Equal(manualList, autoList);
+            
+            testTree.Children[1].MoveNode(1, 0);
+            manualList.RemoveRange(4, 2);
+            manualList.Insert(3, 3);
+            manualList.Insert(4, 4);
+
+            Assert.Equal(manualList.Count, testTree.Count);
+            Assert.Equal(manualList, testTree);
+            Assert.Equal(manualList, autoList);
+            
+            testTree.MoveNode(4, 4);
+
+            Assert.Equal(manualList.Count, testTree.Count);
+            Assert.Equal(manualList, testTree);
+            Assert.Equal(manualList, autoList);
+        }
+
+        [Fact]
+        public void AutoDispatchedReplaceMatchesTree()
+        {
+            var testTree = GenerateTestTree();
+            var autoList = CreateAutoDispatchedList(testTree);
+            var manualList = Enumerable.Range(0, 17).ToList();
+
+            testTree.Children[4].Value = -1;
+            manualList[12] = -1;
+
+            Assert.Equal(manualList.Count, testTree.Count);
+            Assert.Equal(manualList, testTree);
+            Assert.Equal(manualList, autoList);
+            
+            testTree.Children[1].Children[2].Value = -2;
+            manualList[6] = -2;
+
+            Assert.Equal(manualList.Count, testTree.Count);
+            Assert.Equal(manualList, testTree);
+            Assert.Equal(manualList, autoList);
+        }
+
+        [Fact]
+        public void AutoDispatchedReparentingMatchesTree()
+        {
+            var testTree = GenerateTestTree();
+            var autoList = CreateAutoDispatchedList(testTree);
+            var manualList = Enumerable.Range(0, 17).ToList();
+            
+            // 00
+            //     01
+            // 02
+            //     03
+            //         04
+            //     05
+            //     06
+            // 07
+            // 08
+            //     09
+            //         10
+            //             11
+            // 12
+            //     13
+            // 14
+            //     15
+            //     16
+
+            testTree.Children[2].ReparentNode(testTree.Children[3].Children[0], 0);
+            manualList.RemoveAt(8);
+            manualList.Insert(11, 8);
+
+            Assert.Equal(manualList.Count, testTree.Count);
+            Assert.Equal(manualList, testTree);
+            Assert.Equal(manualList, autoList);
+            
+            // 00
+            //     01
+            // 02
+            //     03
+            //         04
+            //     05
+            //     06
+            // 07
+            //     09
+            //         10
+            //             11
+            // 08
+            // 12
+            //     13
+            // 14
+            //     15
+            //     16
+            
+            testTree.Children[1].ReparentNode(testTree.Children[1].Children[0].Children[0], 0);
+            manualList.RemoveAt(4);
+            manualList.Insert(3, 4);
+
+            Assert.Equal(manualList.Count, testTree.Count);
+            Assert.Equal(manualList, testTree);
+            Assert.Equal(manualList, autoList);
+            
+            // 00
+            //     01
+            // 02
+            //     04
+            //     03
+            //     05
+            //     06
+            // 07
+            //     09
+            //         10
+            //             11
+            // 08
+            // 12
+            //     13
+            // 14
+            //     15
+            //     16
+            
+            testTree.Children[1].ReparentNode(testTree.Children[0].Children[0], 0);
+            manualList.RemoveAt(1);
+            manualList.Insert(2, 1);
+
+            Assert.Equal(manualList.Count, testTree.Count);
+            Assert.Equal(manualList, testTree);
+            Assert.Equal(manualList, autoList);
+            
+            
+            // 00
+            // 02
+            //     01
+            //     04
+            //     03
+            //     05
+            //     06
+            // 07
+            //     09
+            //         10
+            //             11
+            // 08
+            // 12
+            //     13
+            // 14
+            //     15
+            //     16
+            
+            testTree.Children[2].Children[0].Children[0].Children[0].ReparentNode(testTree.Children[1], 0);
+            var copy = manualList.Skip(1).Take(6).ToArray();
+            manualList.RemoveRange(1, 6);
+            manualList.InsertRange(5, copy);
+
+            Assert.Equal(manualList.Count, testTree.Count);
+            Assert.Equal(manualList, testTree);
+            Assert.Equal(manualList, autoList);
+            
+            // 00
+            // 07
+            //     09
+            //         10
+            //             11
+            //                 02
+            //                     01
+            //                     04
+            //                     03
+            //                     05
+            //                     06
+            // 08
+            // 12
+            //     13
+            // 14
+            //     15
+            //     16
+            
+            testTree.ReparentNode(testTree.Children[1].Children[0], 0);
+            var copy2 = manualList.Skip(2).Take(9).ToArray();
+            manualList.RemoveRange(2, 9);
+            manualList.InsertRange(0, copy2);
+
+            Assert.Equal(manualList.Count, testTree.Count);
+            Assert.Equal(manualList, testTree);
+            Assert.Equal(manualList, autoList);
+            
+            // 09
+            //     10
+            //         11
+            //             02
+            //                 01
+            //                 04
+            //                 03
+            //                 05
+            //                 06
+            // 00
+            // 07
+            // 08
+            // 12
+            //     13
+            // 14
+            //     15
+            //     16
+        }
+    }
+}
---
+++ LinearTree/LinearTree.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LinearTree
+{
+    public enum CollectionChangeType
+    {
+        INSERT,
+        REMOVE,
+        MOVE,
+        REPLACE
+    }
+
+    public struct CollectionChange
+    {
+        public CollectionChange(CollectionChangeType type, int sourceIndex, int destinationIndex, int count)
+        {
+            Type = type;
+            SourceIndex = sourceIndex;
+            DestinationIndex = destinationIndex;
+            Count = count;
+        }
+        
+        public static CollectionChange Insert(int index, int count) => new CollectionChange(CollectionChangeType.INSERT, -1, index, count);
+        public static CollectionChange Remove(int index, int count) => new CollectionChange(CollectionChangeType.REMOVE, index, -1, count);
+        public static CollectionChange Move(int from, int to, int count) => new CollectionChange(CollectionChangeType.MOVE, from, to, count);
+        public static CollectionChange Replace(int index, int count) => new CollectionChange(CollectionChangeType.REPLACE, index, index, count);
+
+        public CollectionChangeType Type { get; }
+        public int SourceIndex { get; }
+        public int DestinationIndex { get; }
+        public int Count { get; }
+    }
+
+    public struct Reparenting<T>
+    {
+        public LinearTreeNode<T> Node { get; }
+        public LinearTreeNode<T> OldParent { get; }
+        public CollectionChange RemChange { get; }
+        public LinearTreeNode<T> NewParent { get; }
+        public CollectionChange InsChange { get; }
+        public LinearTreeNode<T> MostCommon { get; }
+        public int NodeOffsetRelativeToMostCommonBefore { get; }
+        public int NodeOffsetRelativeToMostCommonAfter { get; }
+
+        public Reparenting(LinearTreeNode<T> node, LinearTreeNode<T> oldParent, CollectionChange remChange, LinearTreeNode<T> newParent,
+            CollectionChange insChange, LinearTreeNode<T> mostCommon, int nodeOffsetRelativeToMostCommonBefore, int nodeOffsetRelativeToMostCommonAfter)
+        {
+            Node = node;
+            OldParent = oldParent;
+            RemChange = remChange;
+            NewParent = newParent;
+            InsChange = insChange;
+            MostCommon = mostCommon;
+            NodeOffsetRelativeToMostCommonBefore = nodeOffsetRelativeToMostCommonBefore;
+            NodeOffsetRelativeToMostCommonAfter = nodeOffsetRelativeToMostCommonAfter;
+        }
+    }
+
+    public interface ILinearTreeNode<T>
+    {
+        IReadOnlyList<LinearTreeNode<T>> Children { get; }
+        LinearTreeNode<T> InsertNode(T value, int index);
+        void RemoveNode(int index);
+        void MoveNode(int from, int to);
+        void ReparentNode(LinearTreeNode<T> node, int index);
+        void ClearChildren();
+    }
+    
+    public class LinearTreeNode<T> : IReadOnlyList<T>, ILinearTreeNode<T>
+    {
+        private readonly List<LinearTreeNode<T>> _children = new List<LinearTreeNode<T>>();
+        private Action<LinearTreeNode<T>, CollectionChange> _dispatchCollectionChange;
+        private readonly int _offset;
+
+        private LinearTreeNode<T> _parent;
+        private int _descendantsCount;
+        private T _value;
+
+        public LinearTreeNode(
+            T value, 
+            Action<LinearTreeNode<T>, CollectionChange> dispatchCollectionChange,
+            LinearTreeNode<T> parent)
+        {
+            _dispatchCollectionChange = dispatchCollectionChange;
+
+            _parent = parent;
+
+            _offset = parent == null ? 0 : 1;
+            
+            Value = value;
+        }
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                
+                if (_parent != null)
+                    _dispatchCollectionChange(this, CollectionChange.Replace(0, 1));
+            }
+        }
+
+        public IReadOnlyList<LinearTreeNode<T>> Children => _children;
+
+        private IEnumerable<T> Iterate()
+        {
+            if (_parent != null)
+                yield return Value;
+            
+            foreach (var child in _children)
+            {
+                foreach (var iterated in child.Iterate())
+                    yield return iterated;
+            }   
+        }
+
+        public IEnumerator<T> GetEnumerator() => Iterate().GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => Iterate().GetEnumerator();
+
+        public int Count => _descendantsCount + _offset;
+        public int Level => _parent?.Level + 1 ?? 0;
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _descendantsCount + _offset)
+                    throw new IndexOutOfRangeException();
+                
+                var cnt = 0;
+
+                if (_parent != null)
+                {
+                    if (index == 0)
+                        return Value;
+
+                    index--;
+                }
+                
+                foreach (var child in _children)
+                {   
+                    var count = child.Count;
+                    if (index < cnt + count)
+                        return child[index - cnt];
+
+                    cnt += count;
+                }
+                
+                throw new IndexOutOfRangeException("Should never happen");
+            }
+        }
+
+        public LinearTreeNode<T> InsertNode(T value, int index)
+        {
+            var node = new LinearTreeNode<T>(value, ReceiveCollectionChange, this);
+            
+            InsertNode(node, index, true);
+
+            return node;
+        }
+
+        private CollectionChange InsertNode(LinearTreeNode<T> node, int index, bool dispatch)
+        {
+            var preCount = 0;
+
+            for (var i = 0; i < index; i++)
+                preCount += _children[i].Count;
+            
+            _children.Insert(index, node);
+            _descendantsCount += node.Count;
+
+            var change = CollectionChange.Insert(preCount + _offset, node.Count);
+            
+            if (dispatch)
+                _dispatchCollectionChange(this, change);
+
+            return change;
+        }
+
+        public void RemoveNode(int index) => RemoveNode(index, true);
+        
+        private CollectionChange RemoveNode(int index, bool dispatch)
+        {
+            var preCount = 0;
+
+            for (var i = 0; i < index; i++)
+                preCount += _children[i].Count;
+            
+            var node = _children[index];
+            var innerCount = node.Count;
+            
+            _children.RemoveAt(index);
+            _descendantsCount -= innerCount;
+
+            var change = CollectionChange.Remove(preCount + _offset, innerCount);
+            
+            if (dispatch)
+                _dispatchCollectionChange(this, change);
+
+            return change;
+        }
+
+        public void MoveNode(int from, int to)
+        {
+            var node = _children[from];
+
+            var positionBefore = 0;
+
+            for (var i = 0; i < from; i++)
+                positionBefore += _children[i].Count;
+            
+            _children.RemoveAt(from);
+            _children.Insert(to, node);
+
+            var positionAfter = 0;
+            
+            for (var i = 0; i < to; i++)
+                positionAfter += _children[i].Count;
+            
+            _dispatchCollectionChange(this, CollectionChange.Move(positionBefore + _offset, positionAfter + _offset, node.Count));
+        }
+
+        public void ReparentNode(LinearTreeNode<T> node, int index)
+        {
+            if (node == this) throw new InvalidOperationException("Self-reparenting is not allowed");
+            if (node._parent == this) throw new InvalidOperationException("Reparenting to same parent is not allowed");
+            EnsureNotParentToChildReparenting(node);
+
+            var mostCommon = node._parent;
+
+            while (mostCommon != null)
+            {
+                var cur = this;
+
+                while (cur != null)
+                {
+                    if (mostCommon == cur)
+                        goto found;
+
+                    cur = cur._parent;
+                }
+
+                mostCommon = mostCommon._parent;
+            }
+             
+            throw new InvalidOperationException("No common parents between current and reparenting node");
+            
+            found:
+
+            var oldParent = node._parent;
+            var oldChildIx = oldParent._children.IndexOf(node);
+
+            var offsetBefore = node.CalculateOffsetRelativeToParent(mostCommon);
+            
+            var remChange = oldParent.RemoveNode(oldChildIx, false);
+
+            if (mostCommon == oldParent)
+            {
+                oldParent._descendantsCount += remChange.Count;
+            }
+            else
+            {
+                var oldParent_ = oldParent._parent;
+                while (oldParent_ != mostCommon && oldParent_ != null)
+                {
+                    oldParent_._descendantsCount -= remChange.Count;
+                    oldParent_ = oldParent_._parent;
+                }
+            }
+
+            node._parent = this;
+            node._dispatchCollectionChange = ReceiveCollectionChange;
+            
+            var insChange = InsertNode(node, index, false);
+
+            if (mostCommon == this)
+            {
+                _descendantsCount -= insChange.Count;
+            }
+            else
+            {
+                var newParent_ = _parent;
+                while (newParent_ != mostCommon && newParent_ != null)
+                {
+                    newParent_._descendantsCount += insChange.Count;
+                    newParent_ = newParent_._parent;
+                }
+            }
+
+            var offsetAfter = node.CalculateOffsetRelativeToParent(mostCommon);
+            mostCommon._dispatchCollectionChange(mostCommon, CollectionChange.Move(offsetBefore, offsetAfter, insChange.Count));
+        }
+
+        private void EnsureNotParentToChildReparenting(LinearTreeNode<T> node)
+        {
+            var parent = _parent;
+            
+            while (parent != null)
+            {
+                if (parent == node)
+                    throw new InvalidOperationException("Reparenting parent to its child is not allowed");
+                
+                parent = parent._parent;
+            }
+        }
+
+        public void ClearChildren()
+        {
+            if (_children.Count is int count && count > 0)
+            {
+                _children.Clear();
+                _dispatchCollectionChange(this, CollectionChange.Remove(_offset, count));
+
+                _descendantsCount = 0;
+            }
+        }
+        
+        private void ReceiveCollectionChange(LinearTreeNode<T> child, CollectionChange change)
+        {
+            var baseIndex = _offset;
+            
+            foreach (var c in _children)
+            {
+                if (c == child)
+                    goto found;
+                    
+                var count = c.Count;
+                baseIndex += count;
+            }
+            
+            return;
+
+            found: ;
+            
+            var newChange = new CollectionChange(
+                change.Type,
+                change.SourceIndex == -1 ? -1 : change.SourceIndex + baseIndex,
+                change.DestinationIndex == -1 ? -1 : change.DestinationIndex + baseIndex, 
+                change.Count);
+
+            switch (change.Type)
+            {
+                case CollectionChangeType.INSERT:
+                    _descendantsCount += change.Count;
+                    break;
+                case CollectionChangeType.REMOVE:
+                    _descendantsCount -= change.Count;
+                    break;
+            }
+
+            _dispatchCollectionChange(this, newChange);
+        }
+
+        private int CalculateOffsetRelativeToParent(LinearTreeNode<T> node)
+        {
+            var offset = 0;
+
+            var parent = _parent;
+            var child = this;
+
+            while (true)
+            {
+                var childIndex = parent._children.IndexOf(child);
+                for (var i = 0; i < childIndex; i++)
+                    offset += parent._children[i].Count;
+
+                offset += parent._offset;
+
+                if (parent == node)
+                    break;
+
+                child = parent;
+                parent = parent._parent ?? throw new Exception("Could not calculate offset: reached root");
+            }
+
+            return offset;
+        }
+    }
+
+    public class LinearTree<T> : IReadOnlyList<T>, ILinearTreeNode<T>
+    {
+        private readonly LinearTreeNode<T> _root;
+        
+        public LinearTree()
+        {
+            _root = new LinearTreeNode<T>(default, ReceiveCollectionChange, null);
+        }
+
+        private void ReceiveCollectionChange(LinearTreeNode<T> node, CollectionChange change)
+        {
+            CollectionChanged?.Invoke(this, change);
+        }
+
+        public IReadOnlyList<LinearTreeNode<T>> Children => _root.Children;
+        public event EventHandler<CollectionChange> CollectionChanged;
+
+        public LinearTreeNode<T> InsertNode(T value, int index) => _root.InsertNode(value, index);
+        public void RemoveNode(int index) => _root.RemoveNode(index);
+        public void MoveNode(int @from, int to) => _root.MoveNode(@from, to);
+        public void ReparentNode(LinearTreeNode<T> node, int index) => _root.ReparentNode(node, index);
+
+        public void ClearChildren() => _root.ClearChildren();
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() => _root.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable) _root).GetEnumerator();
+        public int Count => _root.Count;
+        public T this[int index] => _root[index];
+    }
+}
